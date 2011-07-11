ed31edd1f74b58078696d0a0bb3814ffa2a83685
Jo Bovy <jo.bovy@gmail.com>
2011-07-11 15:56:26 +0000

    segue-local: correct query for possible SEGUE targets

--- sql/possibleSegueTargets.sql
+++ sql/possibleSegueTargets.sql
@@ -1,7 +1,10 @@
 SELECT p.ra, p.dec, p.dered_u as u,p.dered_g as g,p.dered_r as r,
 p.dered_i as i,p.dered_z as z
-into mydb.DBNAME from fGetNearbyObjEq(PLATERA,PLATEDEC,89.4) n, PhotoPrimary p
+into mydb.DBNAME from fGetNearbyObjEq(PLATERA,PLATEDEC,89.4) n, star p
 WHERE n.objID=p.objID
-p.dered_r between 14.5 and 20.2
-and (p.dered_g-p.dered_r) between 0.48 and 0.75
-and (p.flags & (dbo.fPhotoFlags('BRIGHT')+dbo.fPhotoFlags('EDGE')+dbo.fPhotoFlags('BLENDED')+dbo.fPhotoFlags('SATURATED')+dbo.fPhotoFlags('NOTCHECKED')+dbo.fPhotoFlags('NODEBLEND')+dbo.fPhotoFlags('INTERP_CENTER')+dbo.fPhotoFlags('DEBLEND_NOPEAK')+dbo.fPhotoFlags('PEAKCENTER'))) = 0
+and ((p.dered_r between 14.5 and 20.2) or (p.dered_g between 14.5 and 20.2))
+and (p.dered_g-p.dered_r) between 0.2 and 0.75
+and (p.flags & (dbo.fPhotoFlags('SATURATED')+dbo.fPhotoFlags('EDGE')+dbo.fPhotoFlags('INTERP_PSF')+dbo.fPhotoFlags('BADCOUNTS')))= 0
+and ((p.flags & dbo.fPhotoFlags('INTERP_CENTER') = 0) or 
+((p.flags & dbo.fPhotoFlags('INTERP_CENTER') > 0) 
+and (p.flags & dbo.fPhotoFlags('COSMIC_RAY') = 0)))
