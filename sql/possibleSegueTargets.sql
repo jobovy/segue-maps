89c4cd5097b089ee4d1ad587a8de44ad57d2fbe5
Jo Bovy <jo.bovy@gmail.com>
2011-07-11 22:10:26 +0000

    segue-local: SQL query add

--- sql/possibleSegueTargets.sql
+++ sql/possibleSegueTargets.sql
@@ -1,6 +1,6 @@
 SELECT p.ra, p.dec, p.dered_u as u,p.dered_g as g,p.dered_r as r,
 p.dered_i as i,p.dered_z as z
-into mydb.DBNAME from fGetNearbyObjEq(PLATERA,PLATEDEC,89.4) n, Star p
+into mydb.DBNAME from dbo.fGetNearbyObjEq(PLATERA,PLATEDEC,89.4) n, Star p
 WHERE n.objID=p.objID
 and ((p.dered_r between 14.5 and 20.2) or (p.dered_g between 14.5 and 20.2))
 and (p.dered_g-p.dered_r) between 0.2 and 0.75
