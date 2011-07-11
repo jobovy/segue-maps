96519a3978f998be3cdbb9b6cf06812d8246ef08
Jo Bovy <jo.bovy@gmail.com>
2011-07-11 22:07:56 +0000

    segue-local: correct sql query for possible targets

--- sql/possibleSegueTargets.sql
+++ sql/possibleSegueTargets.sql
@@ -1,10 +1,10 @@
 SELECT p.ra, p.dec, p.dered_u as u,p.dered_g as g,p.dered_r as r,
 p.dered_i as i,p.dered_z as z
-into mydb.DBNAME from fGetNearbyObjEq(PLATERA,PLATEDEC,89.4) n, star p
+into mydb.DBNAME from fGetNearbyObjEq(PLATERA,PLATEDEC,89.4) n, Star p
 WHERE n.objID=p.objID
 and ((p.dered_r between 14.5 and 20.2) or (p.dered_g between 14.5 and 20.2))
 and (p.dered_g-p.dered_r) between 0.2 and 0.75
-and (p.flags & (dbo.fPhotoFlags('SATURATED')+dbo.fPhotoFlags('EDGE')+dbo.fPhotoFlags('INTERP_PSF')+dbo.fPhotoFlags('BADCOUNTS')))= 0
-and ((p.flags & dbo.fPhotoFlags('INTERP_CENTER') = 0) or 
-((p.flags & dbo.fPhotoFlags('INTERP_CENTER') > 0) 
-and (p.flags & dbo.fPhotoFlags('COSMIC_RAY') = 0)))
+and (p.flags & (dbo.fPhotoFlags('SATURATED')+dbo.fPhotoFlags('EDGE')+dbo.fPhotoFlags('PSF_FLUX_INTERP')+dbo.fPhotoFlags('BAD_COUNTS_ERROR'))) = 0
+and (((p.flags & dbo.fPhotoFlags('INTERP_CENTER')) = 0) or
+((p.flags & dbo.fPhotoFlags('INTERP_CENTER')) > 0 
+and (p.flags & dbo.fPhotoFlags('COSMIC_RAY')) = 0))
