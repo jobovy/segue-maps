4a8a2b232abdf3a3050ef23f5c31b59ea27435ba
Jo Bovy <jo.bovy@gmail.com>
2011-07-18 12:56:54 +0000

    segue-local: predict and compare number of objects per plate

--- py/compareDataModel.py
+++ py/compareDataModel.py
@@ -148,13 +148,180 @@ def comparerdistPlate(densfunc,params,sf,colordist,data,plate,
                                 lbstr,top_right=True)
         return (rdist, hist[0], hist[1])
 
+def comparernumberPlate(densfunc,params,sf,colordist,data,plate,
+                        rmin=14.5,rmax=20.2,grmin=0.48,grmax=0.55,feh=-0.15,
+                        vsx='|sinb|',
+                        xrange=None,yrange=None,
+                        overplot=False,color='k',marker='v'):
+    """
+    NAME:
+       comparernumberPlate
+    PURPOSE:
+       compare the observed number of objects per plate to the model prediction
+    INPUT:
+       densfunc - underlying density function densfunc(R,z,params)
+       params - array of parameters of the density function
+       sf - segueSelect instance
+       colordist - color g-r distribution
+       data - data recarray (.dered_r is used)
+       plate - plate numbers, or 'all', 'faint', or 'bright'
+       rmin, rmax= minimum and maximum r
+       grmin, grmax= minimum and maximum g-r
+       feh= metallicity to use in d->r
+       vsx= what to plot as x ('b', 'l', 'plate', '|b|', '|sinb|')
+       xrange=, yrange= ranges for plot
+       overplot= if True, overplot
+       color= color for model
+       marker= marker
+    OUTPUT:
+       plot to output
+       return numbers, data_numbers, xs
+    HISTORY:
+       2011-07-18 - Written - Bovy (NYU)
+    """
+    rs= numpy.linspace(rmin,rmax,_NRS)
+    platelb= bovy_coords.radec_to_lb(sf.platestr.ra,sf.platestr.dec,
+                                     degree=True)
+    allplates, faintplates, brightplates = False, False, False
+    if isinstance(plate,str) and plate.lower() == 'all':
+        plate= sf.plates
+        allplates= True
+    elif isinstance(plate,str) and plate.lower() == 'bright':
+        plate= []
+        for ii in range(len(sf.plates)):
+            if not 'faint' in sf.platestr[ii].programname:
+                plate.append(sf.plates[ii])
+        brightplates= True
+    elif isinstance(plate,str) and plate.lower() == 'faint':
+        plate= []
+        for ii in range(len(sf.plates)):
+            if 'faint' in sf.platestr[ii].programname:
+                plate.append(sf.plates[ii])
+        faintplates= True
+    #Set up x
+    if vsx.lower() == 'b' or vsx.lower() == 'l' or vsx.lower() == '|b|' \
+            or vsx.lower() == '|sinb|':
+        platels, platebs= [], []
+        for ii in range(len(plate)):
+            p= plate[ii]
+            #l and b?
+            pindx= (sf.plates == p)
+            platel= platelb[pindx,0][0]
+            plateb= platelb[pindx,1][0]
+            platels.append(platel)
+            platebs.append(plateb)
+        if vsx.lower() == 'b':
+            xs= numpy.array(platebs)
+            xlabel= r'$b\ [\mathrm{deg}]$'
+        elif vsx.lower() == '|b|':
+            xs= numpy.fabs(numpy.array(platebs))
+            xlabel= r'$|b|\ [\mathrm{deg}]$'
+        elif vsx.lower() == '|sinb|':
+            xs= numpy.fabs(numpy.sin(numpy.array(platebs)))
+            xlabel= r'$\sin |b|$'
+        else:
+            xs= numpy.array(platels)
+            xlabel= r'$l\ [\mathrm{deg}]$'
+        addx= 0.1
+    elif vsx.lower() == 'plate':
+        xs= numpy.array(plate)
+        addx= 100
+        xlabel= r'$\mathrm{plate}$'
+    if isinstance(params,list): #list of samples
+        pass
+    else: #single value
+        numbers= numpy.zeros(len(plate))
+        platels, platebs= [], []
+        for ii in range(len(plate)):
+            p= plate[ii]
+            #l and b?
+            pindx= (sf.plates == p)
+            platel= platelb[pindx,0][0]
+            plateb= platelb[pindx,1][0]
+            platels.append(platel)
+            platebs.append(plateb)
+            thisrdist= _predict_rdist_plate(rs,densfunc,params,rmin,rmax,
+                                            platel,
+                                            plateb,grmin,grmax,
+                                            feh,colordist,sf,p)
+            if 'faint' in sf.platestr[pindx].programname[0]:
+                thisrdist[(rs < 17.8)]= 0.
+            else:
+                thisrdist[(rs > 17.8)]= 0.
+            numbers[ii]= numpy.nansum(thisrdist)
+        norm= numpy.nansum(numbers)
+        numbers/= norm
+        if xrange is None:
+            xrange= [numpy.amin(xs)-addx,numpy.amax(xs)+addx]
+        if yrange is None:
+            yrange= [0.,1.2*numpy.amax(numbers)]
+        bovy_plot.bovy_plot(xs,numbers,marker=marker,color=color,ls='none',
+                            xrange=xrange,yrange=yrange,
+                            xlabel=xlabel,
+                            ylabel='$\mathrm{relative\ number}$',
+                            overplot=overplot)
+        #Plot the data
+        data_numbers= []
+        for p in plate:
+            data_numbers.append(numpy.sum((data.plate == p)))
+        data_numbers= numpy.array(data_numbers,dtype='float')
+        nstars= numpy.sum(data_numbers)
+        data_numbers/= nstars
+        bovy_plot.bovy_plot(xs,data_numbers,ls='none',
+                            marker='o',color='k',
+                            overplot=True)
+        if len(plate) > 1 and len(plate) < 10:
+            platestr= '\mathrm{plates}\ \ '
+            for ii in range(len(plate)-1):
+                platestr= platestr+'%i, ' % plate[ii]
+            platestr+= '%i' % plate[-1]
+            lbstr= '$l = %i^\circ \pm %i^\circ$' % (int(numpy.mean(platels)),int(numpy.std(platels)))+'\n'\
+                +'$b = %i^\circ \pm %i^\circ$' % (int(numpy.mean(platebs)),
+            int(numpy.std(platebs)))
+        elif allplates:
+            platestr= '\mathrm{all\ plates}'
+            bovy_plot.bovy_text(r'$'+platestr+'$'
+                                +'\n'+
+                                '$%i \ \ \mathrm{stars}$' % 
+                                nstars,top_right=True)
+        elif brightplates:
+            platestr= '\mathrm{bright\ plates}'
+            bovy_plot.bovy_text(r'$'+platestr+'$'
+                                +'\n'+
+                                '$%i \ \ \mathrm{stars}$' % 
+                                nstars,top_right=True)
+        elif faintplates:
+            platestr= '\mathrm{faint\ plates}'
+            bovy_plot.bovy_text(r'$'+platestr+'$'
+                                +'\n'+
+                                '$%i \ \ \mathrm{stars}$' % 
+                                nstars,top_right=True)
+        elif len(plate) >= 10:
+            platestr= '\mathrm{many\ plates}'
+            lbstr= '$l = %i^\circ \pm %i^\circ$' % (
+                int(numpy.mean(platels)),int(numpy.std(platels)))+'\n'\
+                +'$b = %i^\circ\pm%i^\circ$' % (int(numpy.mean(platebs)),
+            int(numpy.std(platebs)))
+        else:
+            platestr= '\mathrm{plate}\ \ %i' % plate[0]
+            lbstr= '$l = %i^\circ$' % int(platel)+'\n'\
+                +'$b = %i^\circ$' % int(plateb)           
+        if not (allplates or brightplates or faintplates):
+            bovy_plot.bovy_text(r'$'+platestr+'$'
+                                +'\n'+
+                                '$%i \ \ \mathrm{stars}$' % 
+                                nstars
+                                +'\n'+
+                                lbstr,top_right=True)
+        return (numbers, data_numbers, xs)
+    
+
 ###############################################################################
 #            FORWARD MODELING FOR MODEL--DATA COMPARISON
 ###############################################################################
 def _predict_rdist(rs,densfunc,params,rmin,rmax,platelb,grmin,grmax,
                    feh,sf,colordist):
-    """BOVY ADAPT"""
-    """Predict the r distribution for the sample"""
+    """Predict the r distribution for the sample NOT USED"""
     plates= sf.plates
     out= numpy.zeros(len(rs))
     for ii in range(len(plates)):
