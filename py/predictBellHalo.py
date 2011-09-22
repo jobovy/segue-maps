9c748da2b75cbe3160e769cd15f6d8c7df78e93e
Jo Bovy <jo.bovy@gmail.com>
2011-09-22 21:30:14 +0000

    segue-local: mass fraction calculation

--- py/predictBellHalo.py
+++ py/predictBellHalo.py
@@ -1,6 +1,7 @@
 #Predict the number of halo stars in our sample from the Bell et al. (2008) model
 import numpy
 from scipy import integrate
+import isodist
 from segueSelect import segueSelect, read_gdwarfs
 from fitDensz import _const_colordist
 import compareDataModel
@@ -21,3 +22,59 @@ def predictBellHalo():
     bell= 10**8./(integrate.dblquad(lambda r,f: r**2*bellfunc(r*numpy.sin(f),r*numpy.cos(f),None),
                             0.,numpy.pi,lambda x:1., lambda x: 40.)[0]*2.*numpy.pi)
     return numpy.sum(n)*7.*(numpy.pi/180.)**2.*(20.2-14.5)/1000*bell*0.2*numpy.log(10.) #these are some factors left out of compareDataModel
+
+def fracMassGRRange(grmin,grmax,agemin,agemax,FeH):
+    """
+    NAME:
+       fracMassGRRange
+    PURPOSE:
+       calculate the fraction of mass in a range of gr using Padova isochrones
+    INPUT:
+       grmin, grmax -
+       agemin, agemax - in Gyr
+       FeH - metallicity
+    OUTPUT:
+       fraction
+    HISTORY:
+       2011-09-22 - Written - Bovy (IAS)
+    """
+    #Load Padova Isochrones
+    p= isodist.PadovaIsochrone(type='sdss-ukidss',
+                               Z=[0.001+ii*0.001 for ii in range(30)])
+    return massGRRangeInt(grmin,grmax,agemin,agemax,p,FeH,dwarf=True)/\
+           massGRRangeInt(None,None,agemin,agemax,p,FeH)
+
+def massGRRangeInt(grmin,grmax,agemin,agemax,p,FeH,dwarf=False):
+    #Find closest Z
+    Zs= p.Zs()
+    Z= isodist.FEH2Z(FeH)
+    indx= (numpy.abs(Zs-Z)).argmin()
+    logages= p.logages()
+    #Loop over to marginalize
+    indices= (logages >= (9.+numpy.log10(agemin)))*\
+             (logages <= (9.+numpy.log10(agemax)))
+    logages= logages[indices]
+    out= 0.
+    norm= 0.
+    for logage in logages:
+        #Load isochrone
+        iso= p(logage,Z=Zs[indx],asrecarray=True)
+        if not grmin is None:
+            indices= ((iso.g-iso.r) >= grmin)*((iso.g-iso.r) <= grmax)
+            if numpy.sum(indices) == 0:
+                norm+= 10.**(logage-numpy.amin(logages))
+                continue
+            iso= iso[indices]
+        if dwarf:
+            indices= (iso.logg >= 3.5)
+            if numpy.sum(indices) == 0:
+                norm+= 10.**(logage-numpy.amin(logages))
+                continue
+            iso= iso[indices]
+        #Sort on int_IMF and integrate
+        iso= numpy.sort(iso,order='int_IMF')
+        for jj in range(len(iso.M_ini)-1):
+            out+= 0.5*(iso.M_ini[jj]+iso.M_ini[jj+1])*(iso.int_IMF[jj+1]-iso.int_IMF[jj])*10.**(logage-numpy.amin(logages))
+        norm+= 10.**(logage-numpy.amin(logages))
+    print out, norm
+    return out/norm
