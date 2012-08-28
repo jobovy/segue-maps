479a480833068be2f998b2216a6b14028ac65005
Jo Bovy <jo.bovy@gmail.com>
2012-08-28 16:50:07 +0000

    segue-local: note

--- py/pixelFitDF.py
+++ py/pixelFitDF.py
@@ -1,7 +1,8 @@
 #for testing: python pixelFitDF.py --dfeh=0.5 --dafe=0.25 --mcall --mcout --singlefeh=-0.2 --singleafe=0.2 -p 1880 --minndata=1
 #
 # TO DO:
-#   - outlier model sigma (sr=150,sphi=100,sz=100)?
+#   - save normintstuff after setup
+#   - outlier model sigma (sr=150,sphi=100,sz=100)? MAKE SURE QDF IS NORMALIZED-ish
 #   - speed up setting up normintstuff
 #   - include errors
 #
