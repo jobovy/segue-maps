221a1b946e2796486cbe8b0557229c5587241c9c
Jo Bovy <jo.bovy@gmail.com>
2013-01-25 03:26:34 +0000

    segue-local: add interpolated dblexp potential

--- py/pixelFitDF.py
+++ py/pixelFitDF.py
@@ -1508,7 +1508,8 @@ def setup_potential(params,options,npops):
                                              normalize=ampd)
         hp= potential.PowerSphericalPotential(alpha=potparams[4],
                                               normalize=amph)
-        return [dp,hp,bp]
+        #Use an interpolated version for speed
+        return potential.interpRZPotential(RZPot=[dp,hp,bp],rgrid=(numpy.log(0.01),numpy.log(20.),101),zgrid=(0.,1.,101),logR=True,interpepifreq=True,interpverticalfreq=True,use_c=True,enable_c=True,interpPot=True)       
     elif options.potential.lower() == 'mpdiskflplhalofixplfixbulgeflat':
         ro= get_ro(params,options)
         vo= get_vo(params,options,npops)
