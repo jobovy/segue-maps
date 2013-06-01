c6d5632541f443f8ce5374adf4c67464a56b7e4a
Jo Bovy <jo.bovy@gmail.com>
2013-06-01 14:41:07 +0000

    segue-local: more terminal velocity data

--- py/readTerminalData.py
+++ py/readTerminalData.py
@@ -5,4 +5,15 @@ def readClemens():
     glon= data[:,0]
     vterm= data[:,1]
     #Correct for new LSR motion
+    print "BOVY: CORRECT FOR LSR?"
     return (glon,vterm)
+
+def readMcClureGriffiths():
+    data= numpy.loadtxt('McClureGriffiths2007.dat',
+                        comments='#')
+    glon= data[:,0]
+    vterm= data[:,1]
+    #Correct for new LSR motion
+    print "BOVY: CORRECT FOR LSR?"
+    return (glon,vterm)
+    
